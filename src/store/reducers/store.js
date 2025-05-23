baa83e9daacdd66c3efaca9d4cfe28ca3ba89060
Kshitij <kshitijkumar514@gmail.com>
2025-05-23 18:34:58 +0530

    Added the dynamic functionality for Loading state and error state using redux

--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 import { producerReducer } from "./ProductReducer";
+import { errorReducer } from "./errorReducer";
 
 export const store = configureStore({
     reducer:{
         products:producerReducer,
+        errors:errorReducer,
     },
     preloadedState:{},
 });
