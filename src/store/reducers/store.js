61381a39a028ae7ee1b45dd26edeac993a073be5
Kshitij <kshitijkumar514@gmail.com>
2025-06-01 18:31:05 +0530

    Initialized the redux for login page and also added the common Input field file

--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -2,12 +2,19 @@ import {configureStore} from "@reduxjs/toolkit"
 import { producerReducer } from "./ProductReducer";
 import { errorReducer } from "./errorReducer";
 import { cartReducer } from "./cartReducer";
+import { authReducer } from "./authReducer";
 
 const cartItems=localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     :[];
 
+const user=localStorage.getItem("auth")
+    ? JSON.parse(localStorage.getItem("auth"))
+    :[];
+
+
 const initialState={
+    auth:{user:user},
     carts:{cart:cartItems},
 
 };
@@ -17,7 +24,8 @@ export const store = configureStore({
     reducer:{
         products:producerReducer,
         errors:errorReducer,
-        carts:cartReducer
+        carts:cartReducer,
+        auth:authReducer,
     },
     preloadedState:initialState,
 });
